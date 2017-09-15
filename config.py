3743cc3ccd9d69564c298fb53f4cb2f62f828d53
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-09-14 19:48:28 -0500

    Added some comments to the config.

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 
+# List you want to work with
 NUM_LIST = [1,2,2,2,2,2,2,2,3,3,3,3,3,5,5,5,6,6,7,9]
 
+# If it's a sample set or not
 IS_SAMPLE = True
 
+# The action you want to perform
 EQUATION = 'mean'
 
+# Whether or not you want the results to be pretty.
 PRINT_RESULTS = True
